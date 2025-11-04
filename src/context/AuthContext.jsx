191bbec72ad5554ba517fe4203b9011f231dfc43
Helio Bentzen <heliofernando@gmail.com>
2025-11-04 20:02:05 -0300

    Add files via upload

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -6,8 +6,33 @@ const AuthContext = createContext()
 // Provider que envolve a app
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
-    const login = (userData) => setUser(userData);
-    const logout = () => setUser(null);
+
+    async function login(email, password) {
+        /*const data = await apiFetch("/login", {
+            method: "POST",
+            body: JSON.stringify({ email, password })
+        })
+        if (data.token) {
+            localStorage.setItem("token", data.token)
+            setUser({ email })
+        }*/
+
+        const res = await fetch("http://localhost:3001/users");
+        const users = await res.json()
+        const user = users.find(u => u.email === email && u.password === password)
+        if (user) {
+            localStorage.setItem("token", user.token)
+            return true;
+        }
+        return false;
+
+    }
+
+    function logout() {
+        localStorage.removeItem("token")
+        setUser(null);
+    }
+
     return (
         <AuthContext.Provider value={{ user, login, logout }}>
             {children}
